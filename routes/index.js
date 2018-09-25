8bd7ec72076143e1b76681d155cf267258e86506
CarlosCabreraGentile <carloscgentile@gmail.com>
2018-09-25 15:27:32 -0300

    shopping cart button

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -12,6 +13,21 @@ router.get('/', function (req, res, next) {
       productsChunck.push(docs.slice(i, i + chunckSize));
     }
     res.render('shop/index', { title: 'Shopping Cart', products: productsChunck });
+  });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
+
+  Product.findById(productId, function(err, product) {
+    if(err){
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
   });
 });
 
